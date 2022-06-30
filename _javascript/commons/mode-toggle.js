4918e7abd8d88f5b67f9ec266a28aa7b3bfdcd8a
Ferhat Suat Erdoğan <30902468+ekofi@users.noreply.github.com>
2022-06-30 22:03:25 +0300

    p

---
+++ _javascript/commons/mode-toggle.js
@@ -0,0 +1,15 @@
+/*
+ * Listener for theme mode toggle
+ */
+$(function () {
+  $(".mode-toggle").click((e) => {
+    const $target = $(e.target);
+    let $btn =
+      $target.prop("tagName") === "button".toUpperCase()
+        ? $target
+        : $target.parent();
+
+    $btn.blur(); // remove the clicking outline
+    flipMode();
+  });
+});
